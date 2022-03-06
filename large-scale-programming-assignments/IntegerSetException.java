91eb8d736bac3d234eb095a5221d3d5e18c59d03
noahlee24 <70231341+noahlee24@users.noreply.github.com>
2022-03-06 00:10:54 -0500

    Add files via upload

---
+++ large-scale-programming-assignments/IntegerSetException.java
@@ -0,0 +1,11 @@
+package org.howard.edu.lsp.assignment5.implementation;
+
+//
+
+public class IntegerSetException extends Exception
+{
+	 IntegerSetException(String message)
+	{
+		super(message);
+	}
+}
